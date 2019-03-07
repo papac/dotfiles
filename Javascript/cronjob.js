3f0787d7c47c6f8cc798ecda00212dfae21cf43d
Franck DAKIA <dakiafranck@gmail.com>
2019-03-07 18:36:49 +0000

    Update cronjob

--- Javascript/cronjob.js
+++ Javascript/cronjob.js
@@ -1,19 +1,44 @@
+
+/**
+ * Convert Date
+ * 
+ * @param {Number|String} time
+ * @return {Number}
+ */
+function convertDateToTimestemp(time) {
+  if (time === 'now') {
+    return 0;
+  }
+
+  if (time instanceof Date) {
+    return (new Date) - time;
+  }
+
+  return time;
+}
+
+/**
+ * Create the cron job  
+ * 
+ * @param {Object} setting 
+ * @param {Function} task
+ */
 function cronjob(setting, task) {
   let timer;
-  let time = setting.time;
-
-  if (time === 'now') {
-    time = 0;
-  }
+  let isDone = false;
+  let time = convertDateToTimestemp(setting.time || 0);
 
   function done() {
     if (timer) {
       clearTimeout(timer);
+      isDone = true;
     }
   }
 
   timer = setTimeout(() => {
-    task(done);
-    cronjob({time}, task);
+    task(setting.data || {}, done);
+    if (isDone) {
+      cronjob({time}, task);
+    }
   }, time);
 }
