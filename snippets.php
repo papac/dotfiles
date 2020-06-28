d91669b492bf54c310f8fcf92788491d78e9d3b5
Franck DAKIA <dakiafranck@gmail.com>
2020-06-29 01:40:30 +0200

    Create the snippets

---
+++ snippets.php
@@ -0,0 +1,14 @@
+$stats = [
+  ['month' => 'Jan', 'value' => 0],
+  ['month' => 'Fev', 'value' => 0],
+  ['month' => 'Mar', 'value' => 0],
+  ['month' => 'Avr', 'value' => 0],
+  ['month' => 'Mai', 'value' => 0],
+  ['month' => 'Jun', 'value' => 0],
+  ['month' => 'Jul', 'value' => 0],
+  ['month' => 'Aou', 'value' => 0],
+  ['month' => 'Sep', 'value' => 0],
+  ['month' => 'Oct', 'value' => 0],
+  ['month' => 'Nov', 'value' => 0],
+  ['month' => 'Dec', 'value' => 0],
+];
